efbacdc44cb2fdd1f1ccae0e1ed75891e745c99d
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-14 11:37:24 +0200

    Access map is now functionnal

--- src/config/env.example.ts
+++ src/config/env.example.ts
@@ -1,4 +1,5 @@
 export const ENV = {
   BACKEND_API_URL: "https://myurl.com/api/",
-  GOOGLE_ANALYTICS_ID: 'UA-XXXXXXX-X'
+  GOOGLE_ANALYTICS_ID: 'UA-XXXXXXX-X',
+  OPEN_STREET_MAP_ACCESS_TOKEN: 'token'
 };
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { NavController } from 'ionic-angular'
 import L from 'leaflet'
-import ENV from '../../config/env'
+import { ENV } from '../../config/env'
 
 @Component({
   templateUrl: 'map.html'
@@ -18,12 +18,12 @@ export class MapPage {
   
   loadMap() {
   
-    var mymap = L.map('gmap').setView([48.268918, 4.066926], 17);
+    var map = L.map('map', {zoomControl: true}).setView([48.268918, 4.066926], 17);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.streets',
-      accessToken: ENV.OPEN_STREE_MAP_ACCESS_TOKEN
-  }).addTo(mymap);
+      accessToken: ENV.OPEN_STREET_MAP_ACCESS_TOKEN
+  }).addTo(map);
   }
 }
