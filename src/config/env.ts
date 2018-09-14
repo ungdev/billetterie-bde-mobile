34b631a78bb1f30543d563610c03c31f912a05db
Arnaud Dufour <arnaud.dufour@utt.fr>
2018-09-14 11:14:23 +0200

    Delete env.ts

--- src/config/env.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  BACKEND_API_URL: "https://billetterie.bde.utt.fr/api/",
-  GOOGLE_ANALYTICS_ID: 'UA-125040958-1',
-  OPEN_STREE_MAP_ACCESS_TOKEN: 'pk.eyJ1IjoiaGVja3MiLCJhIjoiY2pkcmZ2NTVvMXJlbjMzcW9hMmk0ZWdyMiJ9.EnyIpcTpKkaNXkYgqQ5hHA',
-}
